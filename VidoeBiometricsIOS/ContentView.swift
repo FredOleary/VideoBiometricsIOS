7fdf767333fe8257e503fa5da0981872316b390e
Fred <fred@murphyoleary.com>
2020-01-07 14:52:24 -0800

    Initial Commit

---
+++ VidoeBiometricsIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VidoeBiometricsIOS
+//
+//  Created by Fred OLeary on 1/7/20.
+//  Copyright © 2020 Fred OLeary. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
